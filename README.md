156b8f0ecb2e2466d16b5837b0943a04196039a9
Sam <sikora.samj@gmail.com>
2023-02-02 16:36:17 -0800

    Day 1 Speech with Python

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-## Hour of Code
+# Hour of Code
 
 As a challenge to myself and my friends, I have commited to 100 days of coding for an hour. Here is a log of my day by day and the progress I make:
 
-# Day 1
+### Day 1 
---
+++ Vocal Math Program/VMP_main.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+harvard = sr.AudioFile('Vocal Math Program/audio_files_harvard.wav')
+
+with harvard as source:
+    audio = r.record(source)
+
+print(r.recognize_google(audio, None))
+
+
+
+
